5c683a42d1032d90723622b5729acc2c74a0dcbd
Mikhail Kovalenko <mikov144@mail.ru>
2025-03-16 20:11:20 +0300

    Changes to token expiration

--- backend/cl_back/cl_back/settings.py
+++ backend/cl_back/cl_back/settings.py
@@ -30,8 +30,8 @@ REST_FRAMEWORK = {
 }
 
 SIMPLE_JWT = {
-    "ACCESS_TOKEN_LIFETIME": timedelta(days=1),
-    "REFRESH_TOKEN_LIFETIME": timedelta(days=7),
+    "ACCESS_TOKEN_LIFETIME": timedelta(minutes=30),
+    "REFRESH_TOKEN_LIFETIME": timedelta(days=14),
 }
 
 # Application definition
