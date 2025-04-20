b2573e8b9a16f222f0128da5cd62ca69225a2d73
root <root@xumowvgsqr.local>
2025-04-20 07:59:48 +0000

    Add deployment configuration and necessary modifications

--- backend/cl_back/cl_back/settings.py
+++ backend/cl_back/cl_back/settings.py
@@ -129,7 +129,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
+# Static files (CSS, JavaScript, Images)
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')  # Add this line
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
