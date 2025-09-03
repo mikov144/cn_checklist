3d27f2387aea066658ea9c69eb630033c4902094
Mikhail Kovalenko <mikov144@mail.ru>
2025-09-03 19:57:28 +0300

    Fixed the footer. Did the npm audit fix

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -7,28 +7,21 @@ function Footer() {
         <div className="flex flex-col items-center space-y-3">
           {/* Tech stack info */}
           <div className="flex flex-wrap justify-center items-center gap-2 text-md font-medium text-white/90">
-            <span className="text-cyan-300 font-bold neon-text">v0.34</span>
+            <span className="text-cyan-300 font-bold neon-text">v0.367</span>
             <span className="sm:inline text-white/90">|</span>
-            <span className="text-cyan-300 font-bold neon-text">Django REST framework</span>
+            <span className="text-cyan-300 font-bold neon-text">Django REST</span>
             <span className="sm:inline text-white/90">|</span>
-            <span className="text-cyan-300 font-bold neon-text">Vite + React + TypeScript</span>
-            <span className="sm:inline text-white/90">|</span>
-            <span className="text-cyan-300 font-bold neon-text">Postgresql</span>
+            <span className="text-cyan-300 font-bold neon-text">Vite React</span>
           </div>
 
           {/* Author info */}
           <div className="text-md flex items-center space-x-2 text-white/90 font-medium">
             <span>coded by</span>
-            <a
-              href="https://github.com/mikov144?tab=repositories"
-              target="_blank"
-              rel="noopener noreferrer"
-              className="text-cyan-300 font-retro font-bold transition-all duration-300 neon-text
-                hover:text-cyan-400 hover:scale-110
-                active:scale-95"
+            <span
+              className="text-cyan-300 font-retro font-bold transition-all duration-300 neon-text"
             >
               Mikov
-            </a>
+            </span>
             <span className="text-white/70">2025</span>
           </div>
         </div>
